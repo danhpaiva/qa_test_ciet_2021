49aa7c0b87ef3934efc78df2a7bf2a456a0654de
Paiva <danpaiva@live.com>
2021-06-09 19:59:29 -0300

    Início do primeiro projeto

--- first_scenario/src/test/java/automation_one/AutomateOne.java
+++ first_scenario/src/test/java/automation_one/AutomateOne.java
@@ -1,2 +1,24 @@
-package automation_one;public class AutomateOne {
+package automation_one;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AutomateOne {
+    @Test
+    public void helloWorld() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://opentdb.com/");
+
+        Assert.assertEquals("Open Trivia DB: Free to use, user-contributed trivia question database.", driver.getTitle());
+
+        WebElement buttonBrowse = driver.findElement(By.xpath("//*[@id=\"page-top\"]/section/div/div/div/a[1]"));
+        buttonBrowse.click();
+        
+    }
 }
